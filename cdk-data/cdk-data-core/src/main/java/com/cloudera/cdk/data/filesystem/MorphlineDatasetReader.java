28329160dd7e3a7805381315da38e780bf2a4086
Wolfgang Hoschek <whoschek@cloudera.com>
2013-11-25 01:01:44 -0800

    CDK-231: Add a FileDataSetReader that integrates morphlines

--- cdk-data/cdk-data-core/src/main/java/com/cloudera/cdk/data/filesystem/MorphlineDatasetReader.java
+++ cdk-data/cdk-data-core/src/main/java/com/cloudera/cdk/data/filesystem/MorphlineDatasetReader.java
@@ -15,14 +15,9 @@
  */
 package com.cloudera.cdk.data.filesystem;
 
-import java.beans.IntrospectionException;
-import java.beans.PropertyDescriptor;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -31,10 +26,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.generic.IndexedRecord;
-import org.apache.avro.reflect.ReflectData;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
@@ -57,18 +48,25 @@ import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
 import com.codahale.metrics.Timer;
+import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import com.google.common.io.Closeables;
+import com.google.common.io.Files;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+/**
+ * A DataSetReader that integrates morphlines. The MorphlineDataSetReader opens an HDFS file, runs a
+ * (configurable) pipelined morphline over it, and converts the morphline output to Avro objects,
+ * which are then returned by the DataSetReader pull iterator.
+ */
 public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
 
   private final FileSystem fs;
   private final Path path;
   private final DatasetDescriptor descriptor;
   private final Schema schema;
-  private Class<E> recordClass;
 
   private ReaderWriterState state = ReaderWriterState.NEW;
   private boolean hasLookAhead = false;  
@@ -89,6 +87,7 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
   private final Meter numExceptionRecords;
   
   public static final String MORPHLINE_FILE_PARAM = "morphlineFile";
+  public static final String MORPHLINE_FILE_CONTENTS_PARAM = "morphlineFileContents";
   public static final String MORPHLINE_ID_PARAM = "morphlineId";
   public static final String MORPHLINE_QUEUE_CAPACITY = "morphlineQueueCapacity";
   
@@ -104,47 +103,62 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
 
   
   public MorphlineDatasetReader(FileSystem fileSystem, Path path, DatasetDescriptor descriptor) {
+    this(fileSystem, path, descriptor, null);
+  }
+  
+  public MorphlineDatasetReader(FileSystem fileSystem, Path path, DatasetDescriptor descriptor, 
+      MorphlineContext context) {
+    
     Preconditions.checkArgument(fileSystem != null, "FileSystem cannot be null");
     Preconditions.checkArgument(path != null, "Path cannot be null");
     Preconditions.checkArgument(descriptor != null, "DatasetDescriptor cannot be null");
 
+    //reset(fileSystem, path);
     this.fs = fileSystem;
     this.path = path;
+    this.state = ReaderWriterState.NEW;
+    
     this.descriptor = descriptor;
     this.schema = descriptor.getSchema();
     Preconditions.checkArgument(schema != null, "Schema cannot be null");
     Preconditions.checkArgument(Schema.Type.RECORD.equals(schema.getType()),
         "Schemas for morphline files must be records of primitive types");
     
-    this.state = ReaderWriterState.NEW;
-    this.queue = new ArrayBlockingQueue(getInteger(descriptor, MORPHLINE_QUEUE_CAPACITY, 1000));
+    this.queue = new ArrayBlockingQueue<Record>(getInteger(descriptor, MORPHLINE_QUEUE_CAPACITY, 1000));
     
-    try {
-      this.recordClass = (Class<E>) Class.forName(schema.getFullName());
-    } catch (ClassNotFoundException ignored) {
-      logger.debug("Using generic record, cannot find: {}",
-            schema.getFullName());
-      this.recordClass = null;
-    }
-
     String morphlineFile = descriptor.getProperty(MORPHLINE_FILE_PARAM);
+    String morphlineFileContents = descriptor.getProperty(MORPHLINE_FILE_CONTENTS_PARAM);
+    File tmpMorphlineFile = null;
+    if (morphlineFileContents != null) {
+      try {
+        tmpMorphlineFile = File.createTempFile("morphlines", ".conf");
+        tmpMorphlineFile.deleteOnExit();
+        Files.write(morphlineFileContents, tmpMorphlineFile, Charsets.UTF_8);
+        morphlineFile = tmpMorphlineFile.getPath();
+      } catch (IOException e) {
+        throw new DatasetReaderException(e);
+      }
+    }
     String morphlineId = descriptor.getProperty(MORPHLINE_ID_PARAM);
     if (morphlineFile == null || morphlineFile.trim().length() == 0) {
       throw new MorphlineCompilationException("Missing parameter: " + MORPHLINE_FILE_PARAM, null);
     }
     this.morphlineFileAndId = morphlineFile + "@" + morphlineId;
     
-    FaultTolerance faultTolerance = new FaultTolerance(
-        getBoolean(descriptor, FaultTolerance.IS_PRODUCTION_MODE, false), 
-        getBoolean(descriptor, FaultTolerance.IS_IGNORING_RECOVERABLE_EXCEPTIONS, false),
-        descriptor.getProperty(FaultTolerance.RECOVERABLE_EXCEPTION_CLASSES));
+    if (context == null) {
+      FaultTolerance faultTolerance = new FaultTolerance(
+          getBoolean(descriptor, FaultTolerance.IS_PRODUCTION_MODE, false), 
+          getBoolean(descriptor, FaultTolerance.IS_IGNORING_RECOVERABLE_EXCEPTIONS, false),
+          descriptor.getProperty(FaultTolerance.RECOVERABLE_EXCEPTION_CLASSES));
+      
+      context = new MorphlineContext.Builder()
+        .setExceptionHandler(faultTolerance)
+        .setMetricRegistry(SharedMetricRegistries.getOrCreate(morphlineFileAndId))
+        .build();
+    }   
+    this.morphlineContext = context;
     
-    this.morphlineContext = new MorphlineContext.Builder()
-      .setExceptionHandler(faultTolerance)
-      .setMetricRegistry(SharedMetricRegistries.getOrCreate(morphlineFileAndId))
-      .build();
-    
-    Map morphlineVariables = new HashMap();
+    Map<String, Object> morphlineVariables = Maps.newHashMap();
     for (String name : descriptor.listProperties()) {
         String variablePrefix = MORPHLINE_VARIABLE_PARAM + ".";
         if (name.startsWith(variablePrefix)) {
@@ -156,6 +170,10 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
     this.morphline = new Compiler().compile(new File(morphlineFile), 
         morphlineId, morphlineContext, new Collector(), override);      
     
+    if (tmpMorphlineFile != null) {
+      tmpMorphlineFile.delete();
+    }
+    
     this.mappingTimer = morphlineContext.getMetricRegistry().timer(
         MetricRegistry.name("morphline.app", Metrics.ELAPSED_TIME));
     this.numRecords = morphlineContext.getMetricRegistry().meter(
@@ -165,8 +183,18 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
     this.numExceptionRecords = morphlineContext.getMetricRegistry().meter(
         MetricRegistry.name("morphline.app", Metrics.NUM_EXCEPTION_RECORDS));
   }
+  
+//  private void reset(FileSystem fileSystem, Path path) {
+//    this.fs = fileSystem;
+//    this.path = path;
+//    this.state = ReaderWriterState.NEW;
+//  }
 
   @Override
+  public boolean isOpen() {
+    return (this.state == ReaderWriterState.OPEN);
+  }
+
   @SuppressWarnings("unchecked")
   public void open() {
     Preconditions.checkState(state.equals(ReaderWriterState.NEW),
@@ -206,7 +234,7 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
           try {
             Record record = new Record();
             record.put(Fields.ATTACHMENT_BODY, inputStream);
-            record.put("datasetDescriptor", descriptor);
+            record.put("_dataset_descriptor_schema", schema);
             try {
               Notifications.notifyBeginTransaction(morphline);
               Notifications.notifyStartSession(morphline);
@@ -225,14 +253,14 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
           } finally {
             timerContext.stop();
             Closeables.closeQuietly(inputStream);
-            Notifications.notifyShutdown(morphline);            
+            //Notifications.notifyShutdown(morphline);            
           }
         } finally {
           while (isClosing.getCount() > 0) {
-            // Signal that we've reached EOS
             try {
+              // Signal that we've reached EOS
               if (queue.offer(EOS, 100, TimeUnit.MILLISECONDS)) {
-                break;
+                break; // success - we're done
               }
             } catch (InterruptedException e) {
               ;
@@ -271,6 +299,7 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
     return next;
   }
 
+  @SuppressWarnings("unchecked")
   private E advance() {
     Record morphlineRecord;
     try {
@@ -292,7 +321,8 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
     }
     
     numRecords.mark();
-    return makeRecord(morphlineRecord);
+    Object body = morphlineRecord.getFirstValue(Fields.ATTACHMENT_BODY);    
+    return (E) body;
   }
 
   @Override
@@ -303,7 +333,7 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
     if (!state.equals(ReaderWriterState.OPEN)) {
       return;
     }
-    logger.debug("Closing reader on path:{}", path);
+    logger.debug("Closing reader on path: {}", path);
     state = ReaderWriterState.CLOSED;
     try {
       if (producerThread != null) {
@@ -316,164 +346,6 @@ public class MorphlineDatasetReader<E> extends AbstractDatasetReader<E> {
     }
   }
 
-  @Override
-  public boolean isOpen() {
-    return (this.state == ReaderWriterState.OPEN);
-  }
-
-  private E makeRecord(Record morphlineRecord) {
-    if (recordClass != null) {
-      E record = makeReflectRecord(morphlineRecord);
-      if (record != null) {
-        return record;
-      }
-    }
-    return makeGenericRecord(morphlineRecord);
-  }
-
-  @SuppressWarnings("unchecked")
-  private E makeGenericRecord(Record morphlineRecord) {
-    GenericRecord record = new GenericData.Record(schema);
-    fillIndexed(record, morphlineRecord);
-    return (E) record;
-  }
-
-  @SuppressWarnings("unchecked")
-  private E makeReflectRecord(Record morphlineRecord) {
-    E record = (E) ReflectData.newInstance(recordClass, schema);
-    if (record instanceof IndexedRecord) {
-      fillIndexed((IndexedRecord) record, morphlineRecord);
-    } else {
-      fillReflect(record, morphlineRecord);
-    }
-    return record;
-  }
-
-  private void fillIndexed(IndexedRecord record, Record morphlineRecord) {
-    for (Schema.Field field : record.getSchema().getFields()) {
-      // FIXME support arrays, nested records, etc.
-      String first = (String) morphlineRecord.getFirstValue(field.name());
-      Object value = makeValue(first, field); 
-      record.put(field.pos(), value);
-    }
-  }
-
-  private void fillReflect(Object record, Record morphlineRecord) {
-    for (Schema.Field field : schema.getFields()) {
-      // FIXME support arrays, nested records, etc.
-      String first = (String) morphlineRecord.getFirstValue(field.name());
-      Object value = makeValue(first, field); 
-      try {
-        final PropertyDescriptor propertyDescriptor = new PropertyDescriptor(
-                field.name(), record.getClass(), null, setter(field.name()));
-        propertyDescriptor.getWriteMethod().invoke(record, value);
-      } catch (IntrospectionException ex) {
-        throw new IllegalStateException("Cannot set property " + field.name() +
-            " on " + record.getClass().getName(), ex);
-      } catch (InvocationTargetException ex) {
-        throw new IllegalStateException("Cannot set property " + field.name() +
-            " on " + record.getClass().getName(), ex);
-      } catch (IllegalAccessException ex) {
-        throw new IllegalStateException("Cannot set property " + field.name() +
-            " on " + record.getClass().getName(), ex);
-      }
-    }
-  }
-
-  private static String setter(String name) {
-    return "set" +
-        name.substring(0, 1).toUpperCase(Locale.ENGLISH) +
-        name.substring(1);
-  }
-
-  private static Object makeValue(String string, Schema.Field field) {
-    Object value = makeValue(string, field.schema());
-    if (value != null || nullOk(field.schema())) {
-      return value;
-    } else {
-      // this will fail if there is no default value
-      return ReflectData.get().getDefaultValue(field);
-    }
-  }
-
-  /**
-   * Returns a the value as the first matching schema type or null.
-   *
-   * Note that if the value may be null even if the schema does not allow the
-   * value to be null.
-   *
-   * @param string a String representation of the value
-   * @param schema a Schema
-   * @return the string coerced to the correct type from the schema or null
-   */
-  private static Object makeValue(String string, Schema schema) {
-    if (string == null) {
-      return null;
-    }
-
-    try {
-      switch (schema.getType()) {
-        case BOOLEAN:
-          return Boolean.valueOf(string);
-        case STRING:
-          return string;
-        case FLOAT:
-          return Float.valueOf(string);
-        case DOUBLE:
-          return Double.valueOf(string);
-        case INT:
-          return Integer.valueOf(string);
-        case LONG:
-          return Long.valueOf(string);
-        case ENUM:
-          // TODO: translate to enum class
-          if (schema.hasEnumSymbol(string)) {
-            return string;
-          } else {
-            try {
-              return schema.getEnumSymbols().get(Integer.valueOf(string));
-            } catch (IndexOutOfBoundsException ex) {
-              return null;
-            }
-          }
-        case UNION:
-          Object value = null;
-          for (Schema possible : schema.getTypes()) {
-            value = makeValue(string, possible);
-            if (value != null) {
-              return value;
-            }
-          }
-          return null;
-        default:
-          // FIXED, BYTES, MAP, ARRAY, RECORD are not supported
-          throw new DatasetReaderException(
-              "Unsupported field type:" + schema.getType());
-      }
-    } catch (NumberFormatException ex) {
-      return null;
-    }
-  }
-
-  /**
-   * Returns whether null is allowed by the schema.
-   *
-   * @param schema a Schema
-   * @return true if schema allows the value to be null
-   */
-  private static boolean nullOk(Schema schema) {
-    if (Schema.Type.NULL == schema.getType()) {
-      return true;
-    } else if (Schema.Type.UNION == schema.getType()) {
-      for (Schema possible : schema.getTypes()) {
-        if (nullOk(possible)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-  
   private boolean getBoolean(DatasetDescriptor descriptor, String key, boolean defaultValue) {
     String value = descriptor.getProperty(key);
     if (value != null) {
