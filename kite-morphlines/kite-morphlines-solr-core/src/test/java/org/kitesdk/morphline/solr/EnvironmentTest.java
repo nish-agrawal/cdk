97061e603804b09035fdfc3de8ea437cd0913a5b
Jenkins <dev-kitchen@cloudera.com>
2016-11-28 15:59:50 -0800

    Updating Maven version to 5.10.1-SNAPSHOT

--- kite-morphlines/kite-morphlines-solr-core/src/test/java/org/kitesdk/morphline/solr/EnvironmentTest.java
+++ kite-morphlines/kite-morphlines-solr-core/src/test/java/org/kitesdk/morphline/solr/EnvironmentTest.java
@@ -38,7 +38,7 @@ public class EnvironmentTest extends Assert {
     if (version != null) {
       int i = version.indexOf('-');
       if (i >= 0) {
-        // e.g. strip off "-cdh5.10.0-SNAPSHOT" from "4.10.3-cdh5.10.0-SNAPSHOT"
+        // e.g. strip off "-cdh5.10.1-SNAPSHOT" from "4.10.3-cdh5.10.1-SNAPSHOT"
         version = version.substring(0, i); 
       }
     }
